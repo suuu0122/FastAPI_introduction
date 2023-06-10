b511d91b433be2d75c6a310ae0389df5631e1905
suuu0122 <suuu0122lab@gmail.com>
2023-06-10 23:45:39 +0900

    finish chapter09 Schemas Response, Didn't run out of the code and had to be fixed Dockerfile to use pydantic, See one previous commit for Dockerfile changes

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter
+from typing import List
+import api.schemas.task as task_schema
+
 
 router = APIRouter()
 
-@router.get("/tasks")
+@router.get("/tasks", response_model=List[task_schema.Task])
 async def list_tasks():
-    pass
+    return [task_schema.Task(id=1, title="1つ目のTODOタスク")]
 
 @router.get("/tasks")
 async def create_tasks():
---
+++ api/schemas/task.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class Task(BaseModel):
+    id: int
+    title: Optional[str] = Field(None, example="クリーニングを取りに行く")
+    done: bool = Field(False, description="完了フラグ")
